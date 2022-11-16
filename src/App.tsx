67653b27fd1748bcddc98dc5b0fb8ce9fdab78c3
Steven Mendoza <smendoza171@gmail.com>
2022-11-16 11:05:36 -0800

    added updated Nav

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,8 @@ import CryptoCard from "./components/CryptoCard";
 import FearGreed from "./components/FearGreed/FearGreed";
 import EthGasTracker from "./components/EthGasTracker/EthGasTracker";
 import Converter from "./components/Converter/Converter";
-
+import Footer from "./components/Nav/Footer";
+import Header from "./components/Nav/Header";
 
 const Percent = styled.p<{ data: number }>`
   color: ${(props: any) =>
@@ -67,54 +68,7 @@ function App() {
 
   return (
     <div className="App">
-      <div className="Header">
-        <div className="main-header">
-          <a href="/">
-            <div className="header-title">
-              <img src={logo} />
-              <p>CryptoWidgets</p>
-            </div>
-          </a>
-          <DarkMode />
-        </div>
-
-        <div className="sub-header">
-          <p>
-            Coins: {Number(globalData.active_cryptocurrencies).toLocaleString()}
-          </p>
-          <p>Exchanges: {globalData.markets.toLocaleString()}</p>
-          <div className="market-change">
-            <p>
-              Market Cap: $
-              {globalData.total_market_cap.usd.toLocaleString(undefined, {
-                maximumFractionDigits: 0,
-              })}
-            </p>
-            <Percent data={globalData.market_cap_change_percentage_24h_usd}>
-              {globalData.market_cap_change_percentage_24h_usd.toFixed(2)}%
-              {globalData.market_cap_change_percentage_24h_usd > 0 ? (
-                <i className="fa-solid fa-caret-up"></i>
-              ) : (
-                <i className="fa-solid fa-caret-down"></i>
-              )}
-            </Percent>
-          </div>
-
-          <p>
-            24h Vol: $
-            {Number(globalData.total_volume.usd).toLocaleString(undefined, {
-              maximumFractionDigits: 0,
-            })}
-          </p>
-          <div className="crypto-dominance">
-            <p>Dominance:</p>
-            <p>BTC {globalData.market_cap_percentage.btc.toFixed(1)}%</p>
-            <p>ETH {globalData.market_cap_percentage.eth.toFixed(1)}%</p>
-          </div>
-        </div>
-        {/* <p>Gas: {globalData.active_cryptocurrencies}</p> */}
-      </div>
-
+      <Header globalData={globalData} title={"CryptoWidgets"} />
       <div className="Widgets">
         <Scroller cryptos={cryptos} />
         {/* <CryptoCard cryptos={cryptos} /> */}
@@ -124,6 +78,8 @@ function App() {
         </div>
         <Converter />
       </div>
+      <div className="space"></div>
+      <Footer title={"CryptoWidgets"} />
     </div>
   );
 }
